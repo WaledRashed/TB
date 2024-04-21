3e3b387a3f6c13845c4f0bbc5063877cb1045cbe
Baseta tube <Baseta@users.noreply.huggingface.co>
2024-04-21 04:11:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1538,11 +1538,11 @@ with gr.Blocks(theme=gr.themes.Default(primary_hue="pink", secondary_hue="rose")
                         record_button.change(fn=save_to_wav, inputs=[record_button], outputs=[input_audio0])
                         record_button.change(fn=change_choices2, inputs=[], outputs=[input_audio0])    
                     with gr.Row():
-                        with gr.Accordion('ElevenLabs / Google TTS', open=False, visible=False):
+                        with gr.Accordion('ElevenLabs / Google TTS', open=False, visible=True):
                             with gr.Column():
-                                lang = gr.Radio(label='Chinese & Japanese do not work with ElevenLabs currently.',choices=['ar','en','it','es','fr','pt','zh-CN','de','hi','ja'], value='en')
-                                api_box = gr.Textbox(label="Enter your API Key for ElevenLabs, or leave empty to use GoogleTTS", value='')
-                                elevenid=gr.Dropdown(label="Voice:", choices=eleven_voices)
+                                lang = gr.Radio(label='Chinese & Japanese do not work with ElevenLabs currently.',choices=['ar-eg', 'ar-sa', 'en','it','es','fr','pt','zh-CN','de','hi','ja'], value='en')
+                                api_box = gr.Textbox(label="Enter your API Key for ElevenLabs, or leave empty to use GoogleTTS", value='', visible=False)
+                                elevenid=gr.Dropdown(label="Voice:", choices=eleven_voices, visible=False)
                             with gr.Column():
                                 tfs = gr.Textbox(label="Input your Text", interactive=True, value="This is a test.")
                                 tts_button = gr.Button(value="Speak")
